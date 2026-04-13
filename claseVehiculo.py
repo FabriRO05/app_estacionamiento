3dc9c14f51e375b11617db286b791db1fe57b8d8
FabriRO05 <7b.2018.fabrizio@gmail.com>
2026-04-13 17:15:46 +0200

    main

---
+++ claseVehiculo.py
@@ -0,0 +1,8 @@
+#clases
+class vehiculo:
+    def __init__(self, modelo, marca, puertas, color, patente):
+        self.modelo = modelo
+        self.marca = marca
+        self.puertas = puertas
+        self.color = color
+        self.patente = patente
