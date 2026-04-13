7df4581baf247831968297e3c1496412c6eb6987
FabriRO05 <7b.2018.fabrizio@gmail.com>
2026-04-13 18:14:58 +0200

    mecagoenlap

---
+++ claseUbicacion.py
@@ -0,0 +1,9 @@
+class Ubicacion:
+    def __init__(self, piso, numero, habilitacion):
+        self.piso = piso
+        self.numero = numero
+        self.habilitacion = habilitacion
+        pass
+
+def carecteristicas(self):
+    print(f"Las ubicaciones libres {self.piso},{self.numero},{self.habilitacion}") 
